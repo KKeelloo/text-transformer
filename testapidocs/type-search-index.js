6ed6430bac8047841ee32e8b38f6208bbb0eafc8
Mateusz Ollek <mateuszollek@gmail.com>
2021-01-24 22:39:24 +0000

    Building site for v1.0

---
+++ testapidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"pl.put.poznan.transformer.logic","l":"CapitalizeTest"},{"p":"pl.put.poznan.transformer.logic","l":"ShortcutToStringTest"},{"p":"pl.put.poznan.transformer.logic","l":"StringToShortcutTest"},{"p":"pl.put.poznan.transformer.logic","l":"TextTransformerTest"}]
